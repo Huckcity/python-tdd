5c1f2baaf78a862a83e4612c49ec605e816c1863
Adam Gibbons <adam.p.gibbons@gmail.com>
2019-01-02 14:47:14 +0000

    Improved function

--- evens.py
+++ evens.py
@@ -11,11 +11,8 @@ def even_number_of_evens(numbers):
     
     if count == 0 or count % 2 != 0:
         return False
-    else:
-        return True
         
-        
-    return False
+    return True
 
 assert even_number_of_evens([]) == False, "No numbers"   
 assert even_number_of_evens([2]) == False, "One even number"
